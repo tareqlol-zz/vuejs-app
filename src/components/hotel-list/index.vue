d7020e9b93a54812d68e0ec7eec8c4136b6c4d32
Tareq Hassan <thassan@souq.com>
2017-03-15 15:34:03 +0200

    adding loading indicator for the hotel list while fetching, handling fetch errors with a reload button

--- src/components/hotel-list/index.vue
+++ src/components/hotel-list/index.vue
@@ -1,6 +1,10 @@
 <template>
-  <section class="hotel-list">
+  <section class="hotel-list" v-bind:class="{ 'loading' : fetching }">
     <section is="hotel-item" v-for="hotel in hotelList" v-bind:hotel="hotel"></section>
+    <section v-if="fetchError">
+      There was an error with the request!
+      <button @click="fetchData">Reload</button>
+    </section>
   </section>
 </template>
 
@@ -14,20 +18,35 @@ export default {
   name: 'hotel-list',
   data () {
     return {
-      hotelList : []
+      hotelList : [],
+      fetchError : false
+    }
+  },
+  methods : {
+    // define a fetch data that is used
+    // on created event or from the view
+    // if there are an error
+    fetchData (){
+      this.fetching = true;
+      // fetch the hotel list using vue-resource
+      this.$http.get(serviceUrl).then(response => {
+        // get body data
+        if(response.body && response.body.length){
+          this.hotelList = response.body;
+        }else{
+          this.fetchError = true;
+        }
+        // remove the loading
+        this.fetching = false;
+      }, response => {
+          this.fetchError = true;
+      });
     }
   },
   created () {
-    // fetch the hotel list
-    this.$http.get(serviceUrl).then(response => {
-
-      // get body data
-      this.hotelList = response.body;
-
-    }, response => {
-      
-    });
+    this.fetchData()
   },
+  // define the hotelItem child component 
   components : { hotelItem }
 }
 </script>
