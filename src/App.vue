5e63d75c4e855121c5ae4ee94bf2c0e22c297b18
Tareq Hassan <thassan@souq.com>
2017-03-15 11:10:48 +0200

    defining the app's first layer

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    {{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Vue.js Hotel List'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
